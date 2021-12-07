ff8c981f912d1ed5240375ca8d39577a46849eaa
MinhKhue99 <minhkhuepham992gmail.com>
2021-12-07 09:23:16 +0700

    daily weather features done

--- app/src/main/java/com/minhkhue/weather/ui/MainActivity.kt
+++ app/src/main/java/com/minhkhue/weather/ui/MainActivity.kt
@@ -6,16 +6,24 @@ import android.location.Address
 import android.location.Geocoder
 import android.location.Location
 import android.os.Bundle
+import android.util.Log
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import coil.load
 import coil.transform.CircleCropTransformation
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.minhkhue.weather.R
+import com.minhkhue.weather.apdapter.DailyWeatherAdapter
 import com.minhkhue.weather.databinding.ActivityMainBinding
+import com.minhkhue.weather.model.Daily
 import com.minhkhue.weather.utils.Status
 import com.minhkhue.weather.utils.TimeUtils
 import com.minhkhue.weather.viewmodel.WeatherViewModel
@@ -29,13 +37,16 @@ class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsRes
 	private val weatherViewModel: WeatherViewModel by viewModels()
 	private var latitude: Double = 0.0
 	private var longitude: Double = 0.0
-	private var cityName: String = ""
 	private var fusedLocationProvider: FusedLocationProviderClient? = null
-	
+	private lateinit var weatherAdapter: DailyWeatherAdapter
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		binding = ActivityMainBinding.inflate(layoutInflater)
 		setContentView(binding.root)
+		window.setFlags(
+			WindowManager.LayoutParams.FLAG_FULLSCREEN,
+			WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		supportActionBar?.hide()
 		fusedLocationProvider = LocationServices.getFusedLocationProviderClient(this)
 		if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
 			== PackageManager.PERMISSION_GRANTED
@@ -71,10 +82,13 @@ class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsRes
 			try {
 				val address: MutableList<Address> =
 					geocoder.getFromLocation(location.latitude, location.longitude, 1)
-				latitude = address[0].latitude
-				longitude = address[0].longitude
-				cityName = address[0].locality
-				weatherViewModel.getWeatherData(latitude, longitude)
+				if (address.size > 0) {
+					latitude = address[0].latitude
+					longitude = address[0].longitude
+					var cityName = address[0].locality
+					Log.d("TAG", "getDeviceLocation: $cityName")
+					weatherViewModel.getWeatherData(latitude, longitude)
+				}
 			} catch (ex: IOException) {
 				ex.printStackTrace()
 			}
@@ -85,7 +99,7 @@ class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsRes
 		weatherViewModel.responseWeatherData.observe(this, { response ->
 			when (response.status) {
 				Status.SUCCESS -> {
-					binding.tvLocation.text = cityName
+					//binding.tvLocation.text = cityName
 					binding.tvTemperature.text =
 						StringBuilder(response.data?.current?.temp.toString()).append(" ℃")
 					binding.tvHumidity.text =
@@ -111,6 +125,8 @@ class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsRes
 						response.data.timezone,
 						response.data.current.sunset
 					)
+					setupUI()
+					renderList(response.data.daily)
 				}
 				Status.LOADING -> {}
 				Status.ERROR -> {
@@ -119,6 +135,20 @@ class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsRes
 			}
 			
 		})
+	}
+	
+	private fun renderList(daily: List<Daily>) {
+		weatherAdapter.response = daily
+	}
+	
+	private fun setupUI() {
+		weatherAdapter = DailyWeatherAdapter()
+		binding.rvWeather.apply {
+			adapter = weatherAdapter
+			layoutManager =
+				LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
+			setHasFixedSize(true)
+		}
 	}
 	
 }

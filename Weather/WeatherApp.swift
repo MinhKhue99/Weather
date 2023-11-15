da57b11844fe8c36a52a8bc15f13c63efd5f0e94
KhuePM <minhkhuepham99@gmail.com>
2023-11-15 21:46:10 +0700

    Init Source Project

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Minh Khuê Phạm on 15/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
